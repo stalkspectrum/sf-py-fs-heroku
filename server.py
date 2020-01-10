a046ff514da3a3f03956e411ba426b15df7d4127
stalkspectrum <55536946+stalkspectrum@users.noreply.github.com>
2020-01-10 21:49:57 +0800

    Fix 1 new server and index

--- server.py
+++ server.py
@@ -1,46 +1,19 @@
 import os
 import random
+from bottle import route
+from bottle import run
+from bottle import HTTPError
+from bottle import request
 
-from bottle import route, run
-
-
-def generate_message():
-    return "Сегодня уже не вчера, ещё не завтра"
-
-
-@route("/")
-def index():
-    html = """
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Генератор утверждений</title>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Коллеги, добрый день!</h1>
-      <p>{}</p>
-      <p class="small">Чтобы обновить это заявление, обновите страницу</p>
-    </div>
-  </body>
-</html>
-""".format(
-        generate_message()
-    )
-    return html
+@route('/')
+def server_root():
+    with open('index.html', 'r', encoding='UTF-8') as INDEX_FILE:
+        OUTPUT_ = INDEX_FILE.read()
+    return OUTPUT_
 
 
-@route("/api/roll/<some_id:int>")
-def example_api_response(some_id):
-    return {"requested_id": some_id, "random_number": random.randrange(some_id)}
-
-
-if os.environ.get("APP_LOCATION") == "heroku":
-    run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        server="gunicorn",
-        workers=3,
-    )
-else:
-    run(host="localhost", port=8080, debug=True)
+if __name__ == '__main__':
+    if os.environ.get('APP_LOCATION') == 'heroku':
+        run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), server=gunicorn, workers=2)
+    else:
+        run(host='localhost', port=8080, debug=True)
